2378dd51437c46272c64b7083ccaff8ba8f0f2ac
fuji <fuji@skymizer.com>
2025-11-06 18:31:22 +0800

    rename to test_fp8_5090vllm_docker.sh

--- benchmarks/test_fp8_5090vllm_docker.sh
+++ benchmarks/test_fp8_5090vllm_docker.sh
@@ -13,16 +13,16 @@ compilation-config: '{"cudagraph_mode":"PIECEWISE"}'
 async-scheduling: true
 no-enable-prefix-caching: true
 cuda-graph-sizes: 4096
-max-num-batched-tokens: 16384
-max-model-len: 20480
+max-num-batched-tokens: 16
+max-model-len: 4096
 EOF
 
 export PYTHONNOUSERSITE=1
 
 set -x
-vllm serve $MODEL --host=0.0.0.0 --port=$PORT \
+vllm serve microsoft/Phi-3-mini-4k-instruct --host=0.0.0.0 --port=8888 \
 --config config.yaml \
---gpu-memory-utilization=0.95 \
---tensor-parallel-size=$TP \
---max-num-seqs=$CONC  \
+--gpu-memory-utilization=0.7 \
+--tensor-parallel-size=1 \
+--max-num-seqs=1  \
 --disable-log-requests
